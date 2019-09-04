e91ad701bbc109a689ddfc7acc77eafc881a560a
Yezhong <yezhong.zhang@endowus.com>
2019-09-05 02:01:11 +0800

    update code style

--- gobang/index.js
+++ gobang/index.js
@@ -1,35 +1,39 @@
 // 封装 Gobang 类
-var Gobang = (function() {
+var Gobang = (function () {
 
     // 定义私有变量 roundDirectArr
     // 方向数组
     //[1,1][-1,1][0,1][-1,0]
-    var arr = [1,-1,0]
+    var arr = [1, -1, 0]
     var roundDirectArr = []
     arr.forEach(v => roundDirectArr.push([v, 1]))
-    roundDirectArr.push([-1,0])
+    roundDirectArr.push([-1, 0])
 
     // 定义 Gobang 的方法集
     class GobangUtil {
         //取属性值
-        getProps(x,y) {
-            return 'x'+x+'y'+y
+        getProps(x, y) {
+            return 'x' + x + 'y' + y
         }
 
         //在棋盘取颜色
         getColor(board, x, y) {
-            return board[this.getProps(x,y)]
+            return board[this.getProps(x, y)]
         }
 
         // 返回每个方向的颜色值相同的棋子数
         getNumberOfCoinsInAllDirectionWithSameColor(x, y, board, direct) {
             var result = 0
             var bindGetcolor = this.getColor.bind(this, board)
-            var targetColor = bindGetcolor(x,y)
+            var targetColor = bindGetcolor(x, y)
 
             for (var i = 1; i < 5; i++) {
                 var nextColortoCheck = bindGetcolor(x + i * direct[0], y + i * direct[1])
-                if (targetColor == nextColortoCheck) { result++ } else {break}
+                if (targetColor == nextColortoCheck) {
+                    result++
+                } else {
+                    break
+                }
             }
             return result
         }
@@ -37,7 +41,7 @@ var Gobang = (function() {
         // 判断单个轴(两个方向)是否成立
         checkSingleLineDirection(x, y, board, direct) {
             var leftDirect = direct
-            var rightDirect = direct.map(v=>-v)
+            var rightDirect = direct.map(v => -v)
             var getNum = this.getNumberOfCoinsInAllDirectionWithSameColor.bind(this, x, y, board)
 
             return (getNum(leftDirect) + 1 + getNum(rightDirect)) >= 5
@@ -51,10 +55,10 @@ var Gobang = (function() {
 
     // 定义 Gobang
     class Gobang extends GobangUtil {
-        constructor(callback){
+        constructor(callback) {
             super()
-            var board = new Proxy({},{
-                get: function(target, property) {
+            var board = new Proxy({}, {
+                get: function (target, property) {
                     if (property in target) {
                         return target[property];
                     } else {
@@ -81,7 +85,7 @@ var Gobang = (function() {
 })()
 
 
-var Game = (function() {
+var Game = (function () {
     var chessColor = 2
     var isWin = false
     var ele = document.getElementById('root')
@@ -107,29 +111,29 @@ var Game = (function() {
             htm = '',
             arr = [...Array(size).keys()]
 
-            rwidth = ele.clientWidth
-            // console.log(rwidth)
-            swidth = parseInt((rwidth/size), 10) + 'px'
-            cwidth = parseInt((rwidth/size/1.2), 10) + 'px'
+        rwidth = ele.clientWidth
+        // console.log(rwidth)
+        swidth = parseInt((rwidth / size), 10) + 'px'
+        cwidth = parseInt((rwidth / size / 1.2), 10) + 'px'
 
-        tpl= (coord, color) => ( `
+        tpl = (coord, color) => (`
             <div class="square" style="width:${swidth};height:${swidth}">
                 <div class="circle ${color ? color : ''}" data-coord="${coord}" style="width:${cwidth};height:${cwidth}"></div>
             </div>
         `)
 
-        arr.forEach(v => arr.forEach(e => htm += tpl([e,v])))
+        arr.forEach(v => arr.forEach(e => htm += tpl([e, v])))
 
         return htm
     }
 
-    function renderHtm (tpl) {
+    function renderHtm(tpl) {
         ele.innerHTML = tpl
     }
 
     function bindEvent(mygobang) {
-        ele.addEventListener('click', (e)=> {
-            if(isWin) {
+        ele.addEventListener('click', (e) => {
+            if (isWin) {
                 return
             }
             var target = e.target
@@ -143,12 +147,12 @@ var Game = (function() {
     }
 
     return {
-        start(size,{onEnd,onKeep}) {
+        start(size, {onEnd, onKeep}) {
             // 渲染棋盘
             renderHtm(renderTpl(size))
 
             // 生成 gobang 实例并绑定事件
-            bindEvent(instantGobang(onEnd,onKeep))
+            bindEvent(instantGobang(onEnd, onKeep))
         }
     }
 })()
